7c05e4c4100b3e47a13c9b38c572d358351ebf43
Stas2107 <163865519+Stas2107@users.noreply.github.com>
2024-08-15 19:42:11 +0400

    1

--- flower_shop/flower_shop/urls.py
+++ flower_shop/flower_shop/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  # Подключаем URL из приложения accounts
+    path('register/', include('accounts.urls')),
 ]
