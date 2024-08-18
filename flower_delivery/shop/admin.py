9c5409765ef0a15e05a6c3555b9b755b890e90ef
Stas2107 <163865519+Stas2107@users.noreply.github.com>
2024-08-18 14:16:12 +0400

    1

--- flower_delivery/shop/admin.py
+++ flower_delivery/shop/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Product, Order
 
-admin.site.register(Product)
-admin.site.register(Order)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'image')  # Добавляем поле image
+    search_fields = ('name',)
+    list_filter = ('price',)
+    fields = ('name', 'price', 'image')  # Указываем порядок полей в форме
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at')
+    search_fields = ('user__username',)
+    list_filter = ('created_at',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
