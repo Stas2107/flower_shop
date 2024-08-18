1550485c43691ce5e222cf7c92316b46c95045a6
Stas2107 <163865519+Stas2107@users.noreply.github.com>
2024-08-18 12:50:54 +0400

    1

--- flower_shop/accounts/urls.py
+++ flower_shop/accounts/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 from . import views
-from .views import register, login, index, catalog, ContactView, order_view, order_success, index_view
+from .views import register, login, catalog, ContactView, order_view, order_success_view, index_view
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('', index_view, name='index'),
+    path('', index_view, name='index'),  # Главная страница
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('catalog/', catalog, name='catalog'),
     path('contact/', ContactView.as_view(), name='contact'),
-    path('order/', order_view, name='order'),
-    path('order/success/', order_success, name='order_success'),
-    path('order/<int:product_id>/', order_view, name='order'),
+    path('order/<int:product_id>/', order_view, name='order'),  # Оформление заказа для конкретного продукта
+    path('order/success/', order_success_view, name='order_success'),  # Успешное оформление заказа
 ]
