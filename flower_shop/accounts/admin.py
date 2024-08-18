303570cccae44cdad9661cf470c9a08373855dae
Stas2107 <163865519+Stas2107@users.noreply.github.com>
2024-08-18 13:33:43 +0400

    1

--- flower_shop/accounts/admin.py
+++ flower_shop/accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import accounts
+from .models import Accounts
 
 # Регистрация модели Flower в админке
-admin.site.register(accounts)
+admin.site.register(Accounts)
--- flower_shop/accounts/models.py
+++ flower_shop/accounts/models.py
@@ -1,35 +1,21 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 
-class accounts(models.Model):
+# Модель для хранения информации о цветах
+class Flower(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.URLField()  # Здесь вы можете использовать URL изображения (например, от внешнего хоста)
+    image = models.URLField()  # URL изображения
 
-    def __str__(self):
+    def __str__(self):  # Исправлено на __str__
         return self.name
 
-
-
-
-# механика заказов----------
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image_url = models.URLField()
-
-    def __str__(self):
-        return self.name
-
+# Модель для хранения заказов
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
+    flowers = models.ManyToManyField(Flower)  # Изменено на Flower
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def __str__(self):  # Исправлено на __str__
         return f'Order {self.id} by {self.user.username}'
